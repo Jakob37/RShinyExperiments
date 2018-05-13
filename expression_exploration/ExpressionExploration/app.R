025b4c6bb6862b674014bd82dcd69bb8ba48c5cd
Jakob Willforss <jakob.willforss@hotmail.com>
2018-05-13 10:21:06 +0200

    Starting outlining file upload

--- expression_exploration/ExpressionExploration/app.R
+++ expression_exploration/ExpressionExploration/app.R
@@ -4,19 +4,14 @@ library(tidyverse)
 source("~/src/JupyterRReuse/MultivarVis.R")
 source("~/src/JupyterRReuse/EvalVis.R")
 
+# Data upload
+# https://gist.github.com/dgrapov/dfcf6ab2339b1cf19b090cfb8dadc855
+
 ddf <- read_tsv("data/bull_design.tsv")
 rdf <- read_tsv("data/bull_data.tsv")
 sdf <- rdf[, ddf$sample]
 
-print(colnames(rdf))
-print(ddf$sample)
-print(!which(colnames(rdf) %in% ddf$sample))
-
 non_sample_head <- colnames(rdf)[which(!colnames(rdf) %in% ddf$sample)]
-
-print(non_sample_head)
-
-# feature_col <- rdf[, "Feature"]
 cond_col <- colnames(ddf)
 
 ui <- fluidPage(
@@ -25,6 +20,14 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
+      wellPanel(
+        fileInput("data_fp", "Choose data file",
+                  multiple=FALSE,
+                  accept=c("text/tsv", ".tsv")),
+        fileInput("design_fp", "Choose data file",
+                  multiple=FALSE,
+                  accept=c("text/tsv", ".tsv"))
+      ),
       wellPanel(
         selectInput("plot_type", 
                     "Plot type:", 
@@ -45,6 +48,19 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  
+  # get_data <- reactive({
+  #   
+  #   req(input$data_fp)
+  #   
+  #   infile <- input$data_fp
+  #   if (is.null(data_fp)) {
+  #     return(NULL)
+  #   }
+  #   objects_loaded <- load(input$data_fp$name)
+  #   df <- eval(parse(text=objects_loaded[1]))
+  #   return(df)
+  # })
   
   output$plot <- renderUI({
     
@@ -134,8 +150,11 @@ server <- function(input, output) {
   
   output$pca <- renderPlot({
     
+    #print(head(get_data()))
+    
     mv$pca(sdf, unlist(ddf[, input$color_factor]), 
            ddf$sample, ddf$sample, pc1=input$pc1, pc2=input$pc2, only_text=F)
+    
   }, width="auto", height="auto")
   
   output$dendogram <- renderPlot({
